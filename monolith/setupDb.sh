5591f7770055a78a38d09392b6336123f911c600
amitsatpute-gslab <amitkumar.satpute@gslab.com>
2023-02-24 21:09:09 +0530

    database setup file added

--- monolith/setupDb.sh
+++ monolith/setupDb.sh
@@ -1,2 +1,10 @@
+CYAN=$(tput setaf 6)
+normal=$(tput sgr0)
+
+printf "${CYAN}Creating Database\n${normal}"
 npm run createDb
+printf "${CYAN}Database created\n${normal}"
+
+printf "${CYAN}Running migrations\n${normal}"
 npm run migrate
+printf "${CYAN}Migration done\n\n${normal}"
--- monolith/setupMockDB.sh
+++ monolith/setupMockDB.sh
@@ -1,3 +1,14 @@
+CYAN=$(tput setaf 6)
+normal=$(tput sgr0)
+
+printf "${CYAN}Creating Database\n${normal}"
 npm run createDb
+printf "${CYAN}Database created\n${normal}"
+
+printf "${CYAN}Running migrations\n${normal}"
 npm run migrate
+printf "${CYAN}Migration done\n${normal}"
+
+printf "${CYAN}Adding mockdata\n${normal}"
 node src/database/insertMockData.js
+printf "${CYAN}Mockdata added successfully...\n\n${normal}"
