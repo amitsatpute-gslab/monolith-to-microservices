59555b7a69b5120d21e89e092383b3f08e36381b
shahtejas-t <tejas.p.shaha@gmail.com>
2023-08-29 20:24:20 +0530

    Modifying data for incremental primary key for kafka data sync

--- monolith/src/database/migrations/20230224093104_users.js
+++ monolith/src/database/migrations/20230224093104_users.js
@@ -4,7 +4,7 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
-    table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
+    table.increments('id').primary().unsigned();
     table.string("full_name").notNullable();
     table.string("address");
     table.string("email");
--- monolith/src/database/migrations/20230224093313_orders.js
+++ monolith/src/database/migrations/20230224093313_orders.js
@@ -4,9 +4,9 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable("orders", (table) => {
-    table.uuid("id").primary().defaultTo(knex.raw("(UUID())"));
-    table.json("product_ids");
-    table.uuid("user_id").references("id").inTable("users").notNullable();
+    table.increments('id').primary().unsigned();
+    table.string("product_ids");
+    table.integer("user_id").unsigned().references("id").inTable("users").notNullable();
     table.float("total_cost").defaultTo(0);
     table.timestamp("created_at").defaultTo(knex.fn.now());
     table.timestamp("updated_at").defaultTo(knex.fn.now());
